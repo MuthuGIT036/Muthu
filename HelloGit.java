9221629352ebcdd967448230047f07f4be8afad7
root <root@ip-172-31-0-237.ap-south-1.compute.internal>
2026-09-22 06:37:28 +0000

    Add Git change message

--- HelloGit.java
+++ HelloGit.java
@@ -2,5 +2,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("This is my first Java project using Git.");
+        System.out.println("I made my first Git change.");
     }
 }
